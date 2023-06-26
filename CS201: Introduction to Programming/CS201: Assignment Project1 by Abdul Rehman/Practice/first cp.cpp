9c655636a2a893bf11d81bac4012d5696cfdc89b
Abdul Rehman <105493274+AbdulRehmanGHub@users.noreply.github.com>
2023-06-26 10:46:24 +0500

    practice programming

---
+++ CS201: Introduction to Programming/CS201: Assignment Project1 by Abdul Rehman/Practice/first cp.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+	int a, b;
+int num1, num2;
+char y;
+
+num1= b;
+num2 = a;
+
+cin >> num1;
+cout <<"here is your 1st number ";
+
+cin >> num2;
+cout <<"here is your 2nd number";
+
+}
---
+++ CS201: Introduction to Programming/CS201: Assignment Project1 by Abdul Rehman/Practice/test.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+main()
+{
+	int a, b;
+
+cout <"press 1 for a ";
+
+cout <"1";
+		
+}
+
